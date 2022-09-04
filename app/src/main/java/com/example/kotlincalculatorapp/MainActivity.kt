6ec41597b656bf3e57031ab850fdcf6face453c9
Light House <yvoshngumi@gmail.com>
2022-09-04 23:54:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kotlincalculatorapp/MainActivity.kt
@@ -0,0 +1,47 @@
+  package com.example.kotlincalculatorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+  class MainActivity : AppCompatActivity() {
+
+      private var canAddOperation = false
+      private var canAddDecimal = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+      fun allClearAction(view: View)
+      {
+          workingsTV.text = ""
+          resultsTV.text = ""
+      }
+      fun backSpaceAction(view: View)
+      {
+          val length = workingsTV.length()
+          if(length > 0)
+              workingTV.text = workingTv.text.subSequence(0, length - 1)
+      }
+      fun equalAction(view: View) {}
+
+      fun numberAction(view: View)
+      {
+          if(view is Button){
+              workingsTV.append(view.text)
+              canAddOperation = true
+          }
+          }
+
+      fun operationalAction(view: View)
+      {
+          if(view is Button){
+              workingsTV.append(view.text && canAddOperation)
+              canAddOperation = false
+          }
+      }
+      }
